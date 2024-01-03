5acea32b9c8424b64ec7f4610fdde7c4c6e17028
Onni Hinkkanen <onni.u.i.hinkkanen@gmail.com>
2024-01-03 14:39:12 +0200

    first few functions

--- tehtavat.js
+++ tehtavat.js
@@ -11,6 +11,35 @@ function gcd(a, b) {
     return a;
 }
 
+function murtolukujenSumma(a,b,c,d){
+    let osoittaja = a*d + b*c;
+    let nimittaja = b*d;
+    let jakaja = gcd(osoittaja, nimittaja);
+    while (jakaja > 1){
+        osoittaja = osoittaja / jakaja;
+        nimittaja = nimittaja / jakaja;
+        jakaja = gcd(osoittaja, nimittaja)
+    }
+    return [osoittaja, nimittaja];
+}
 
+function formatoi_vastaus(vast) {
+    let temp = vast;
+    if (temp.includes("=")){
+        temp = temp.substring(1);
+    }
+    if (!temp.includes("/")){
+        console.log("Ei murtoluku!")
+        return;
+    }
+    const arr = temp.split("/");
 
-test();
+    console.log(arr[0]);
+    console.log(arr[1]);
+    return arr;
+}
+
+let vastArr = formatoi_vastaus("=-32/17");
+let oikArr = murtolukujenSumma(1,2,3,2);
+console.log(vastArr[0])
+console.log(vastArr[1])

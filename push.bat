b0783027931ac071003ed3c164c7d8b167cba326
Onni Hinkkanen <onni.u.i.hinkkanen@gmail.com>
2024-01-10 15:39:56 +0200

    modified push script

--- push.bat
+++ push.bat
@@ -1,19 +1,23 @@
-echo kissa
-
-
+SLEEP 1
 git add --all
-git commit -m "%1"
+SLEEP 1
+git commit -m "%*"
+SLEEP 1
 git push
 
-
+SLEEP 5
 git checkout public
+SLEEP 1
 git checkout main -- propedjs.js
+SLEEP 1
 git checkout main -- push.bat
+
+SLEEP 1
 git add --all
-git commit -m "%1"
+SLEEP 1
+git commit -m "%*"
+SLEEP 1
 git push
 
-
+SLEEP 5
 git checkout main
-
-pause

fcf4d8df19300a989f64512f3a5548d359381739
Onni Hinkkanen <onni.u.i.hinkkanen@gmail.com>
2024-01-10 15:31:29 +0200

    push script

---
+++ push.bat
@@ -0,0 +1,19 @@
+echo kissa
+
+
+git add --all
+git commit -m "%1"
+git push
+
+
+git checkout public
+git checkout main -- propedjs.js
+git checkout main -- push.bat
+git add --all
+git commit -m "%1"
+git push
+
+
+git checkout main
+
+pause

ecb52796d60793678b32d96b1db2a126fe8c2ece
Onni Hinkkanen <onni.u.i.hinkkanen@gmail.com>
2024-01-10 15:29:45 +0200

    testi

--- push.bat
+++ push.bat
@@ -1,6 +1,18 @@
 echo kissa
 
+REM push to private git
 git add --all
 git commit -m "%1"
 git push
+
+REM push the .js file to public git
+git checkout public
+git checkout main -- propedjs.js
+git add --all
+git commit -m "%1"
+git push
+
+REM get back to main
+git checkout main
+
 pause

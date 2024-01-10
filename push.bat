2c682459b887ceee0fe69595cc13941f6a4a2ce0
Onni Hinkkanen <onni.u.i.hinkkanen@gmail.com>
2024-01-10 15:41:04 +0200

    modified push script

--- push.bat
+++ push.bat
@@ -1,23 +1,23 @@
-SLEEP 1
+timeout /t 1
 git add --all
-SLEEP 1
+timeout /t 1
 git commit -m "%*"
-SLEEP 1
+timeout /t 1
 git push
 
-SLEEP 5
+timeout /t 5
 git checkout public
-SLEEP 1
+timeout /t 1
 git checkout main -- propedjs.js
-SLEEP 1
+timeout /t 1
 git checkout main -- push.bat
 
-SLEEP 1
+timeout /t 1
 git add --all
-SLEEP 1
+timeout /t 1
 git commit -m "%*"
-SLEEP 1
+timeout /t 1
 git push
 
-SLEEP 5
+timeout /t 5
 git checkout main

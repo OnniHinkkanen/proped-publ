05bfa8e83d3d121b0ac5ddb378b9dbbac6372425
Onni Hinkkanen <onni.u.i.hinkkanen@gmail.com>
2024-01-10 15:37:32 +0200

    testi

--- push.bat
+++ push.bat
@@ -1,13 +1,23 @@
+SLEEP 1
 git add --all
+SLEEP 1
 git commit -m "%1"
+SLEEP 1
 git push
 
+SLEEP 5
 git checkout public
+SLEEP 1
 git checkout main -- propedjs.js
+SLEEP 1
 git checkout main -- push.bat
 
+SLEEP 1
 git add --all
+SLEEP 1
 git commit -m "%1"
+SLEEP 1
 git push
 
+SLEEP 5
 git checkout main

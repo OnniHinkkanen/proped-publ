d5c80d871a958322df79fbbe31394f9e5007d139
Onni Hinkkanen <onni.u.i.hinkkanen@gmail.com>
2024-01-11 15:43:07 +0200

    further updated publishing script

--- publish.bat
+++ publish.bat
@@ -8,30 +8,30 @@ IF NOT "%*"=="" (
 
 taskkill /f /im Code.exe
 
-timeout /t 1
+timeout /t 1 > nul:
 git add --all
-timeout /t 1
+timeout /t 1 > nul:
 git commit -m "%msg%"
-timeout /t 1
+timeout /t 1 > nul:
 git push
 
-timeout /t 5
+timeout /t 3 > nul:
 git checkout public
-timeout /t 1
+timeout /t 1 > nul:
 git checkout main -- propedjs.js
-timeout /t 1
+timeout /t 1 > nul:
 git checkout main -- publish.bat
 
-timeout /t 1
+timeout /t 1 > nul:
 git add --all
-timeout /t 1
+timeout /t 1 > nul:
 git commit -m "%msg%"
-timeout /t 1
+timeout /t 1 > nul:
 git push
 
-timeout /t 5
+timeout /t 3 > nul:
 git checkout main
 
-timeout /t 1
+timeout /t 1 > nul:
 
 start c:\"Program Files"\"Microsoft VS Code"\Code.exe

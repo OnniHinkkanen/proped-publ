ef9442007926c1545d89ad47f23842e97924bcf5
Onni Hinkkanen <onni.u.i.hinkkanen@gmail.com>
2024-01-12 13:03:40 +0200

    decimal plus and minus work

--- propedjs.test.js
+++ propedjs.test.js
@@ -181,6 +181,41 @@ describe("decSumFeedback", () => {
     expect(decSumFeedback(0.705, 0.365, '=1,071')).toBe(virhe);
   });
 
+  test('0.11 + 0.23 to be 0.34', () => {
+    expect(decSumFeedback('0.11', '0.23', '0.34')).toBe("Oikein!");
+  });
+
+  test('0.11 + 0.23 to be 0.34', () => {
+    expect(decSumFeedback('0.114', '0.483', '0,597')).toBe("Oikein!");
+  });
+  
+  test('-0.11 + 0.23 to be 0.12', () => {
+    expect(decSumFeedback('-0.11', '0.23', '0.12')).toBe("Oikein!");
+  });
+
+  test('0.105 + 0.365 to be 0.470', () => {
+    expect(decSumFeedback('0.105', '0.365', '=0,470')).toBe("Oikein!");
+  });
+
+  test('0.705 + 0.365 to be 1.070', () => {
+    expect(decSumFeedback('0.705', '0.365', '=1,070')).toBe("Oikein!");
+  });
+
+  test('0.705 + 0.365 to be 1.07', () => {
+    expect(decSumFeedback('0.705', '0.365', '=1,07')).toBe("Oikein!");
+  });
+
+  test('0.705 + 0.365 should not be 1.071', () => {
+    expect(decSumFeedback('0.705', '0.365', '=1,071')).toBe(virhe);
+  });
+
+  test('0.705 - 0.365 to be 0.34', () => {
+    expect(decSumFeedback(0.705, -0.365, '=0.34')).toBe("Oikein!");
+  });
+  test('0.223 - 0.365 to be -0.142', () => {
+    expect(decSumFeedback(0.223, -0.365, '=-0.142')).toBe("Oikein!");
+  });
+
 });
 
 describe("stringReplace", () => {

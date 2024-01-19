5f48fe93efcb88b02450dd715de0ad45a56175b9
Onni Hinkkanen <onni.u.i.hinkkanen@gmail.com>
2024-01-19 10:06:17 +0200

    publish

--- publish.sh
+++ publish.sh
@@ -27,28 +27,3 @@ git push
 git checkout main
 
 code
-
-
-#timeout /t 3 
-#git checkout public
-#timeout /t 1 
-#git checkout main -- propedjs.js
-#timeout /t 1
-#git checkout main -- publish.bat
-#
-#timeout /t 1 
-#git add --all
-#timeout /t 1 
-#git commit -m "%msg%"
-#timeout /t 1 
-#git push
-#
-#timeout /t 3 
-#git checkout main
-#
-#timeout /t 1 
-#
-#start c:\windows\system32\cmd.exe
-#\timeout /t 1
-#start c:\"Program Files"\"Microsoft VS Code"\Code.exe
-#exit

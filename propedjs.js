86c40de8cc5f7b84ff36043d8d120891f0fb81fd
Onni Hinkkanen <onni.u.i.hinkkanen@gmail.com>
2024-02-06 13:05:05 +0200

    publish

--- propedjs.js
+++ propedjs.js
@@ -117,7 +117,7 @@ function decSumFeedback(a,b,ans) {
  * @param {number/string} a 1st decimal number
  * @param {number/string} b 2nd decimal number
  * @param {string} ans user answer
- * @return feedback string
+ * @return {string} feedback string
  */
 function decProdFeedback(a,b,ans) {
     return decFeedback(a,b,ans, f = (x,y) => x*y);
@@ -232,25 +232,20 @@ function approxEq(v1, v2, epsilon = 0.0001) {
     return Math.abs(v1 - v2) < epsilon;
   };
 
-
-
-
-
-
-
 /**
  * Replaces all the ocurrences of oldChar with newChar
  *
  * @param {string} string 
  * @param {string} oldChar 
  * @param {string} newChar 
+ * @returns {string} the new string
  */
 function stringReplace(string, oldChar, newChar){
     if (!string.includes(oldChar)) {
         return string;
     }
-    newString = "";
-    for (i = 0; i < string.length; i++){
+    let newString = "";
+    for (let i = 0; i < string.length; i++){
         char = string.charAt(i)
         if (char != oldChar){
             newString = newString + char;
@@ -261,7 +256,240 @@ function stringReplace(string, oldChar, newChar){
     return newString;
 }
 
+/**
+ * Evaluates if the user answer is correct for product of two similar powers
+ *
+ * @param {number} a 1st exponend
+ * @param {number} b 2nd exponent
+ * @param {string} vari variable
+ * @param {string} ans what the user answered
+ * @return {string} feedback for the student answer
+ */
+function prodofpowers(a, b, vari, ans){
+    return power(a,b,vari, ans, f =(x,y) => x+y)
+}
+
+/**
+ * Evaluates if the user answer is correct for power of power
+ *
+ * @param {number} a 1st exponend
+ * @param {number} b 2nd exponent
+ * @param {string} vari variable
+ * @param {string} ans what the user answered
+ * @return {string} feedback for the student answer 
+ */
+function powerofpower(a, b, vari, ans){
+    return power(a,b,vari, ans, f =(x,y) => x*y)
+}
+
+
+
+/**
+ *A function for evaluating the responses of students for exponentiation rules. 
+ *
+ * @param {*} a 1st exponent
+ * @param {*} b 2nd exponent
+ * @param {*} vari variable; base number
+ * @param {*} ans what the student answered
+ * @param {*} f function to evaluate the exponent; e.g. a^5*a^3 would need f= (x,y) => x + y; to obtain the correct answer a^8.
+ * @return {string} feedback for the student answer 
+ */
+function power(a,b,vari,ans, f){
+    let tempAns = ans.replace('=', '').replace('(','').replace(')','');
+    if (!tempAns.includes(vari)){
+        if (f(a,b) == 0 && parseInt(tempAns) == 1){
+            return oikein;
+        }
+        return virhe;
+    }
+    let arr = tempAns.split('^');
+    if (parseInt(arr[1]) == f(a,b)){
+        return oikein;
+    }
+    return virhe;
+}
+
+/**
+ * Evaluates if the user answer is correct for quotinent of two similar powers
+ *
+ * @param {number} a 1st exponend
+ * @param {number} b 2nd exponent
+ * @param {string} vari variable
+ * @param {string} ans what the user answered
+ */
+function quotofpowers(a, b, vari, ans){
+    return power(a,b,vari, ans, f =(x,y) => x - y)
+}
+
+function split_to_polynomials(str){
+
+    const better_regex = /\)\(|\)(?=\d|[a-zA-Z])|(?<=\d|[a-zA-Z])\(/
+
+    //matches ')(' and splits the string into array
+    const regex = /(?<=\))\(/;
+
+    //Remove all white space chars and asterixes and split with the regex
+    let arr = str.replace(/[\s \*]+/g, '').split(better_regex);
+
+    //splits the array further into subarrays for + and -
+    for (let i = 0; i < arr.length; i++){
+        arr[i] = arr[i].replace(/[\(\)]/g, '')
+        let temparr = arr[i].split(/([\+\-]\d*[a-zA-Z]*)/g) //[\+\-]\d*
+        arr[i] = temparr.filter(n => n)
+    }
+    //array of arrays of monomials
+    return arr;
+}
+
+function sort_by_power(array) {
+
+}
+
+
+/**
+ *A class for handling polynomial products ads summation. Currently only works with single-variable polynomials, 
+ *but extending this to multi-variable polynomials should not be hard at all.
+ *
+ * @class Polynomial
+ */
+class Polynomial{
+    constructor(variable, coefficients){
+        this.variable = variable;
+        this.coefficients = coefficients;
+    }
+
+    
+
+    /**
+     * Let 
+     * p(x) = p_n x^n + ... + p_1 x + p_0 and
+     * q(x) = q_m x^m + ... + q_1 x + q_0.
+     * 
+     * Now
+     * p(x)q(x) = \sum_{k=0}^{n+m} \sum_{l = 0}^k p_l q_{k-l} x^k
+     *          = \sum_{k=0}^{n+m} x^k \sum_{l = 0}^k p_l q_{k-l}.
+     *
+     * Hence we need to calculate 
+     * 
+     * \sum_{l = 0}^k p_l q_{k-l}
+     * 
+     * for k \in \{0, 1, ... , n + m\} to attain the product coefficients.
+     * 
+     * This only works, if the polynomials are of the same variable. If we have 
+     * 
+     * p(x) = p_0 + p_1 x + ... + p_n x^n and
+     * q(y) = q_0 + q_1 x + ... + q_m y^m
+     * 
+     * and want to calculate p(x)q(y), the coefficients would form the n x m matrix
+     * 
+     * p_0q_0   p_0q_1  ... p_0q_m
+     * p_1q_0   p_1q_1         :
+     *   :              ...    .
+     * p_nq_m   ...         p_nq_m
+     * 
+     * where the indices denote the power of the corresponding variable x and y, respectively.
+     * 
+     * This is yet to be implemented
+     * 
+     * @param {Polynomial} a the other polynomial to be multiplied
+     * @returns {Polynomial} the polynomial product as a member of Polynomial
+     * @memberof Polynomial
+     */
+    times(a){
+        if (this.variable == a.variable){
+            let p = this.coefficients;
+            let q = a.coefficients;
+            let n = this.coefficients.length - 1;
+            let m = a.coefficients.length - 1;
+            let product = [];
+            
+            // Construct the sums
+            for (let k = 0; k <= n + m; k++){
+                let sum = 0;
+                for (let l = 0; l <= k; l++){
+    
+                    // Check that the indexes are in range
+                    if (l <= n && k - l <= m){
+                        sum += p[l]*q[k - l]
+                    }
+                }
+                product.push(sum);
+            }
+    
+            return new Polynomial(this.variable, product);
+        }
+        else {
+            const e = new Error("Multi-variable product is yet to be implemented")
+            e.name = "NotImplementedError"
+            throw e;
+        }
+    }
+
+
+    /**
+     * Addition for polynomials. Currently only works for polynomials of the same variable.
+     *
+     * @param {Polynomial} a the other polynomial
+     * @return {Polynomial} Resulting polynomial of the summation 
+     * @memberof Polynomial
+     */
+    plus(a){
+        if (this.variable == a.variable){
+            let lb = Math.min(this.coefficients.length, a.coefficients.length);
+            let sum = []
+            for(let i = 0; i < lb; i++){
+                sum.push(this.coefficients[i] + a.coefficients[i])
+            }
+
+            if (this.coefficients.length > lb){
+                for (let i = lb; i < this.coefficients.length; i++){
+                    sum.push(this.coefficients[i])
+                }
+                return sum;
+            }else {
+                for (let i = lb; i < a.coefficients.length; i++){
+                    sum.push(a.coefficients[i])
+                }
+                return new Polynomial(this.variable, sum);
+            }
+        } else {
+            const e = new Error("Multi-variable product is yet to be implemented")
+            e.name = "NotImplementedError"
+            throw e;
+        }
+    }   
+
+
+    /**
+     *Polynomial substraction acquired by summation; p(x) + (-q(x)). 
+     * Currently only works for polynomials of the same variable.
+     *
+     * @param {Polynomial} a polynomial to be substracted i.e. q(x)
+     * @return {Polynomial} The resulting polynomial 
+     * @memberof Polynomial
+     */
+    minus(a){
+        return this.plus(new Polynomial(a.variable, a.coefficients.map((e) => -1*e)));
+    }
+}
+
+
 // ------------------ Variables END -----------------------------------
+// The line above is due to TIM integration; everything below will not get exported to TIM.
+
+//let a = split_to_polynomials('(3*x^2 + 2x - 1)(3x +4)')
+//console.log(a[0]+ " " + a[1])
+
+//console.log(split_to_polynomials("(2x-3)(3x-2)"))
+
+let a = new Polynomial('x', [-2,1])
+let b = new Polynomial('x', [3,2,5])
+console.log(a.times(b).coefficients)
+//console.log(a.plus(b))
+//console.log(a.minus(b))
+
+//let polytest = new Polynomial('a', [1,2,3]);
+//console.log(polytest.coefficients);
 
 module.exports = {
     gcd,
@@ -275,83 +503,21 @@ module.exports = {
     decSumFeedback,
     decProdFeedback,
     stringReplace,
+    power,
+    prodofpowers,
+    quotofpowers,
+    powerofpower,
     sievennys,
     virhe,
     oikein,
-    syoteVirhe
+    syoteVirhe,
+    Polynomial,
 };
 
-console.log(decFeedback('0.705', '0.365', '=1,070', f = (x,y) => x + y));
-console.log(decFeedback('0.245', '0.483', '0,118335', f= (x,y) => x*y));
+
+//console.log(bintimesbin('(2x - 1)(3x +4)','x'))
+
+//console.log(decFeedback('0.705', '0.365', '=1,070', f = (x,y) => x + y));
+//console.log(decFeedback('0.245', '0.483', '0,118335', f= (x,y) => x*y));
 
 // ================================================= OLD JUNK =====================================================
-function olddecSumFeedback(a,b,ans){
-    let tempAns = String(ans),
-        pa = a,
-        pb = b;
-
-    if (typeof a != 'number'){
-        pa = parseFloat(a);
-    }
-
-    if (typeof b != 'number'){
-        pb = parseFloat(b);
-    }
-
-    if (tempAns.includes("=")){
-        tempAns = tempAns.substring(1);
-    }
-
-    pans = parseFloat(tempAns.replace(',','.'));
-
-    if (isNaN(pans) || isNaN(pa) || isNaN(pb) ){
-        return syoteVirhe;
-    }
-    
-    let sum = pa + pb;
-
-    if (approxEq(sum, pans)) {
-        return oikein;
-    }
-
-    return virhe;
-}
-
-/**
- * 
- *
- * @param {number} a 1st dec number
- * @param {number} b 2nd dec number
- * @param {string} ans what the user entered
- */
-function olddecProdFeedback(a,b,ans){
-    let tempAns = String(ans),
-    pa = a,
-    pb = b;
-
-if (typeof a != 'number'){
-    pa = parseFloat(a);
-}
-
-if (typeof b != 'number'){
-    pb = parseFloat(b);
-}
-
-if (tempAns.includes("=")){
-    tempAns = tempAns.substring(1);
-}
-
-pans = parseFloat(tempAns.replace(',','.'));
-
-if (isNaN(pans) || isNaN(pa) || isNaN(pb) ){
-    return syoteVirhe;
-}
-
-let prod = pa*pb;
-
-if (approxEq(prod, pans)) {
-    return oikein;
-}
-
-return virhe;
-}
